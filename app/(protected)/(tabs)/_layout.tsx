4432924bd73d97558c40284bc291417c41ced4b5
Raheem Adebayo <adraheemzy@gmail.com>
2026-01-28 02:05:49 +0100

    tab bar icons

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -1,5 +1,13 @@
 import { useColors } from "@/hooks/use-colors";
-import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Octicons from "@expo/vector-icons/Octicons";
+import {
+  Icon,
+  Label,
+  NativeTabs,
+  VectorIcon,
+} from "expo-router/unstable-native-tabs";
 
 export default function TabLayout() {
   const colors = useColors();
@@ -7,19 +15,28 @@ export default function TabLayout() {
   return (
     <NativeTabs backBehavior="none" tintColor={colors.primary}>
       <NativeTabs.Trigger name="index">
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
+        <Icon src={<VectorIcon family={AntDesign} name="home" />} />
         <Label>Home</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="movies">
-        <Icon sf="gear" drawable="custom_settings_drawable" />
+        <Icon
+          src={<VectorIcon family={MaterialCommunityIcons} name="movie-roll" />}
+        />
         <Label>Movies</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="tv">
-        <Icon sf="gear" drawable="custom_settings_drawable" />
+        <Icon
+          src={
+            <VectorIcon
+              family={MaterialCommunityIcons}
+              name="television-classic"
+            />
+          }
+        />
         <Label>Series</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="search" role="search">
-        <Icon sf="gear" drawable="custom_settings_drawable" />
+        <Icon src={<VectorIcon family={Octicons} name="search" />} />
         <Label>Search</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
